3c137905907a51bb67336cfd87f81e875ee2ac00
Frank Celler <f.celler@triagens.de>
2015-01-26 16:45:23 +0100

    do not use fg

--- scripts/mesos/agency.sh
+++ scripts/mesos/agency.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+(
+  echo " ---> Waiting 5 seconds..."
+
+  sleep 5
+
+  echo " ---> Filling agency with life..."
+
+  arangosh --javascript.execute /scripts/mesos/init_agency.js
+
+  echo " ---> Done with initializing"
+) &
+
 echo " ---> starting agency on slave $HOST"
 
-/usr/libexec/arangodb/etcd-arango --data-dir /data -name agentarango4001 &
-
-echo " ---> Waiting 5 seconds..."
-
-sleep 5
-
-echo " ---> Filling agency with life..."
-
-arangosh --javascript.execute /scripts/mesos/init_agency.js
-
-echo " ---> Done, foregrounding etcd."
-
-fg %1
+exec /usr/libexec/arangodb/etcd-arango --data-dir /data -name agentarango4001
