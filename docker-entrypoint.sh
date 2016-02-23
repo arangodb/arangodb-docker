928ade17edf3b867f5e23b3ce8ea691274f5da5b
Andreas Streichardt <andreas@arangodb.com>
2016-02-23 15:01:27 +0100

    ensure we have proper rights on our volumes the "soft" way

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,12 +6,20 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- arangod "$@"
 fi
 
+function rwfail {
+  echo "We seem to not have proper rw access to $1. Please make sure that every mounted volume has full rw access for user arangodb ($(id arangodb))"
+  exit 55
+}
+
 if [ "$1" = 'arangod' ]; then
 	DATADIR=/var/lib/arangodb
 	
         # ensure proper uid and gid (for example when volume is mounted from the outside)
-        chown -R arangodb:arangodb "$DATADIR"
-	chown -R arangodb:arangodb /var/lib/arangodb-apps
+        # do NOT chown or stuff like that. when using shared folders on the mac chown is VERY likely to fail due to docker => vm => host issues
+        touch "$DATADIR"/_rwcheck_$HOSTNAME || rwfail $DATADIR
+        rm "$DATADIR"/_rwcheck_"$HOSTNAME"
+        touch /var/lib/arangodb-apps/_rwcheck_"$HOSTNAME" || rwfail /var/lib/arangodb-apps/
+        rm /var/lib/arangodb-apps/_rwcheck_"$HOSTNAME"
 	if [ ! -f "$DATADIR"/SERVER ]; then
 		if [ -z "$ARANGO_ROOT_PASSWORD" -a -z "$ARANGO_NO_AUTH" -a -z "$ARANGO_RANDOM_ROOT_PASSWORD" ]; then
 			echo >&2 'error: database is uninitialized and password option is not specified '
