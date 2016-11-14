4a7654b091307c3b806e6ac2ae89eca6fcefb45c
Wilfried Goesgens <willi@arangodb.com>
2016-11-14 17:04:57 +0100

    add script to build the official docker library repository file

---
+++ build_docker_library_file.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+while [ $# -gt 0 ];  do
+    case "$1" in
+        --isMajorRelease)
+            shift
+            NEW_MAJOR_RELEASE=$1
+            shift
+            ;;
+        --gitBranch)
+            shift
+            GITBRANCH=$1
+            shift
+            ;;
+        --gitTag)
+            shift
+            GITTAG=$1
+            shift
+            ;;
+        --releaseVersion)
+            shift
+            RELEASE_VERSION=$1
+            shift
+            ;;
+        *)
+            echo "don't understand $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${NEW_MAJOR_RELEASE}" == "true" ];  then
+    cat library/arangodb \
+        | grep -v "${GITBRANCH}" \
+        | grep -v latest > /tmp/docker_library_template
+else
+    cat library/arangodb \
+        | grep -v "${GITBRANCH}" > /tmp/docker_library_template
+fi
+
+echo "${GITBRANCH}: git://github.com/arangodb/arangodb-docker@${RELEASE_VERSION} jessie/${GITTAG}" >> /tmp/docker_library_template
+echo "${GITTAG}: git://github.com/arangodb/arangodb-docker@${RELEASE_VERSION} jessie/${GITTAG}" >> /tmp/docker_library_template
+
+if grep -q latest /tmp/docker_library_template ; then
+    echo "This is not our newest branch. - thus this is not going to change the LATEST entry."
+else
+    echo "Adding this version as the LATEST entry:"
+    echo "latest: git://github.com/arangodb/arangodb-docker@${RELEASE_VERSION} jessie/${GITTAG}" >> /tmp/docker_library_template
+fi
+
+echo "the official docker library file 'library/arangodb' is now: "
+cat /tmp/docker_library_template
+
+# now we send the pull request to the official docker library:
+# ############################################################
+cp /tmp/docker_library_template library/arangodb
