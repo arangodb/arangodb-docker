5e3113040163a9c02df8c2aac06b0f3b750450d7
Max Neunhoeffer <max@arangodb.com>
2018-07-03 08:21:44 +0200

    Reintroduce +x for empty passwords.

--- stretch/3.3.10/docker-entrypoint.sh
+++ stretch/3.3.10/docker-entrypoint.sh
@@ -37,7 +37,7 @@ if [ "$1" = 'arangod' ]; then
         if [ -f "$ARANGO_ROOT_PASSWORD_FILE" ]; then
             ARANGO_ROOT_PASSWORD="$(cat $ARANGO_ROOT_PASSWORD_FILE)"
         fi
-        if [ -z "${ARANGO_ROOT_PASSWORD}" ] && [ -z "$ARANGO_NO_AUTH" ] && [ -z "$ARANGO_RANDOM_ROOT_PASSWORD" ]; then
+        if [ -z "${ARANGO_ROOT_PASSWORD+x}" ] && [ -z "$ARANGO_NO_AUTH" ] && [ -z "$ARANGO_RANDOM_ROOT_PASSWORD" ]; then
             echo >&2 'error: database is uninitialized and password option is not specified '
             echo >&2 "  You need to specify one of ARANGO_ROOT_PASSWORD, ARANGO_NO_AUTH and ARANGO_RANDOM_ROOT_PASSWORD"
             exit 1
@@ -50,7 +50,7 @@ if [ "$1" = 'arangod' ]; then
             echo "==========================================="
         fi
         
-        if [ ! -z "${ARANGO_ROOT_PASSWORD}" ]; then
+        if [ ! -z "${ARANGO_ROOT_PASSWORD+x}" ]; then
             echo "Initializing root user...Hang on..."
             ARANGODB_DEFAULT_ROOT_PASSWORD="$ARANGO_ROOT_PASSWORD" /usr/sbin/arango-init-database -c /tmp/arango-init-database.conf || true
             export ARANGO_ROOT_PASSWORD
